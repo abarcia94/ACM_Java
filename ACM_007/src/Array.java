5f450c0e8be0f136de40b6386dd3653416281b27
abarcia94 <a.barcia@alumnos.upm.es>
2016-10-13 16:51:02 +0200

    Funcion es par

--- ACM_007/src/Array.java
+++ ACM_007/src/Array.java
@@ -1,20 +1,28 @@
 import java.util.Scanner;
 
-
 public class Array {
 	static String[] clases = {"Clase 1", "Clase 2", "Clase 3", "Clase 4", "Clase 5"};
+	static int[] cadena = new int[50];
+	static int a;
+	
+	public static void esPar() {
+		if (cadena[a] % 2 == 0){
+			System.out.println( "Posicion : " + a + " - " + cadena[a]);
+		}
+	}
 	public static void main(String[] ar) {
-		int[] cadena = new int[50];
 		
-		for (int a= 0; a < cadena.length; a++){
+		for (a= 0; a < cadena.length; a++){
 			cadena[a]= a+1;
+			esPar();
 			
 			//System.out.println( "Posicion : " + a + " - " + cadena[a] + " - " + clases[a]);
-			if (cadena[a] % 2 == 0){
+			/*if (cadena[a] % 2 == 0){
 				System.out.println( "Posicion : " + a + " - " + cadena[a]);
-			}
+			}*/
 			
 		}
+		
 		
 	}
 
