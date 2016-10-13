a25190fabc0b85219e3a9ad208026a53775ceaac
abarcia94 <a.barcia@alumnos.upm.es>
2016-10-13 16:23:29 +0200

    Mejora

--- ACM_007/src/Array.java
+++ ACM_007/src/Array.java
@@ -4,12 +4,16 @@ import java.util.Scanner;
 public class Array {
 	static String[] clases = {"Clase 1", "Clase 2", "Clase 3", "Clase 4", "Clase 5"};
 	public static void main(String[] ar) {
-		int[] cadena = new int[5];
+		int[] cadena = new int[50];
 		
 		for (int a= 0; a < cadena.length; a++){
 			cadena[a]= a+1;
 			
-			System.out.println( "Posicion : " + a + " - " + cadena[a] + " - " + clases[a]);
+			//System.out.println( "Posicion : " + a + " - " + cadena[a] + " - " + clases[a]);
+			if (cadena[a] % 2 == 0){
+				System.out.println( "Posicion : " + a + " - " + cadena[a]);
+			}
+			
 		}
 		
 	}
